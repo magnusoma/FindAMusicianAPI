d8732f616ba61229b9df7f5f8f171e5abaae20fe
PetterW <petter.wibstad@icloud.com>
2021-04-26 17:01:21 +0200

    added fk and pk

--- Models/Artist.cs
+++ Models/Artist.cs
@@ -4,5 +4,50 @@ namespace FindAMusician.Models {
     public class Artist {
         [Key]
         public int ArtistID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+        public string Bio { get; set; }
+        public int UpVote { get; set; }
+        public int DownVote { get; set; }
+        public IList<ArtistMusicanType> ArtistMusicanType { get; set; }
+    }
+    public class ArtistMusicanType {
+        [Key]
+        public int ArtistID { get; set; }
+        public Artist Artist { get; set; }
+        public int MusicanTypeID { get; set; }
+        public MusicanType MusicanType { get; set; }
+    }
+
+    public class MusicanType {
+        [Key]
+        public int MusicanTypeID { get; set; }
+        public int Type { get; set; }
+        public IList<ArtistMusicanType> ArtistMusicanType { get; set; }
     } 
 }
+
+/*
+
+Artist
+
+Artist_ID {PK}
+Name
+Price
+Image
+Bio
+UpVote
+DownVote
+
+Artist-Musican-Type
+
+ID {Pk, FK}
+MT-ID {PK, FK}
+
+Musician-Type
+
+ID {PK}
+Type 
+
+*/
