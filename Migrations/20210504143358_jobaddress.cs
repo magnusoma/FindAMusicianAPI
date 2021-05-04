ec2e2da964873f4d6d43593f7044419765c0b06b
PetterW <petter.wibstad@icloud.com>
2021-05-04 16:36:20 +0200

    fix jobaddress in controller

---
+++ Migrations/20210504143358_jobaddress.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FindAMusicianAPI.Migrations
+{
+    public partial class jobaddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
